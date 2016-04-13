60e028bb790344db7ea1a0bc8fa4916213d12830
Naomi Porto <naomiporto95@gmail.com>
2016-04-13 17:05:39 +0200

    Modificacion del css, añadiendo navbar, creando una pagina de about y cambiando el estilo

--- djangogirls_site/urls.py
+++ djangogirls_site/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'djangogirls_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^about/', TemplateView.as_view(template_name="about.html"), name="about"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-    url('^', include('django.contrib.auth.urls'))
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url('^', include('django.contrib.auth.urls')),
+    
+    
  ]
