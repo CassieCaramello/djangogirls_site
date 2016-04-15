c922e43559970712978d736525ec4db6cb9a104e
Naomi Porto <naomiporto95@gmail.com>
2016-04-15 13:46:12 +0200

    Añadiendo class a las views

--- blog/views.py
+++ blog/views.py
@@ -3,47 +3,66 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostList(ListView):
+    model = Post
+    context_object_name = 'posts'
+    def get_queryset(self):
+        queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return queryset
+#def post_list(request):
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #return render(request, 'blog/post_list.html', {'posts': posts})
 
+class PostDetail(DetailView):
+    model = Post
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = Comment.objects.filter(post=post).order_by('created_date')
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     comments = Comment.objects.filter(post=post).order_by('created_date')
     
-    if request.method == "POST":
-        form = CommentForm(request.POST)
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
 
-        if form.is_valid():
-            if request.user.is_authenticated():
-                comment = form.save(commit=False)
-                comment.author = request.user
-                comment.post = post
-                comment.save()
-                return redirect('blog.views.post_detail', pk=post.pk)
-            else:
-                return redirect('login')
-    else:
-        form = CommentForm()
+#         if form.is_valid():
+#             if request.user.is_authenticated():
+#                 comment = form.save(commit=False)
+#                 comment.author = request.user
+#                 comment.post = post
+#                 comment.save()
+#                 return redirect('blog.views.post_detail', pk=post.pk)
+#             else:
+#                 return redirect('login')
+#     else:
+#         form = CommentForm()
 
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form':form})
+#     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form':form})
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'blog/post_edit.html'
+    success_url = '/'
 
+    def form_valid(self, form):
+        
+        form.instance.author = self.request.user
+        form.instance.published_date = timezone.now()
+        return super(PostCreate, self).form_valid(form)
 
-@login_required(login_url='login')    
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.publish()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+# @login_required(login_url='login')    
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.publish()
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required(login_url='login')
 def post_edit(request, pk):
--- djangogirls_site/urls.py
+++ djangogirls_site/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'djangogirls_site.views.home', name='home'),
