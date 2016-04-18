66a7757d6b087113f4bfede2c56b03c443f51859
Naomi Porto <naomiporto95@gmail.com>
2016-04-18 13:42:09 +0200

    Poner comentarios en los post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
-from .forms import PostForm, CommentForm
+from .forms import CommentForm
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
@@ -12,19 +12,39 @@ class LoginRequiredMixin(object):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view, login_url='login')
 
-        
+
 class PostList(ListView):
     model = Post
     context_object_name = 'posts'
     def get_queryset(self):
         queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
         return queryset
-#def post_list(request):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    #return render(request, 'blog/post_list.html', {'posts': posts})
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(post=self.object).order_by('-created_date')
+        context['form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            form = CommentForm(request.POST)
+            post = self.get_object()
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.author = request.user
+                comment.post = post
+                comment.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            return redirect('login') 
 
 # def post_detail(request, pk):
 #     post = get_object_or_404(Post, pk=pk)
@@ -46,7 +66,7 @@ class PostDetail(DetailView):
 #         form = CommentForm()
 
 #     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form':form})
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'text']
     template_name = 'blog/post_edit.html'
